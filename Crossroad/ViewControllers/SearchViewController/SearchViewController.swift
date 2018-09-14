82ea0f084d0d2886204d4d9f3ac7cc18d77d134c
Juri <juri@Juris-MBP-2.connect>
2018-09-14 21:59:03 +0100

    Project initial structure setup

---
+++ Crossroad/ViewControllers/SearchViewController/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchViewController.swift
+//  Crossroad
+//
+//  Created by Juri on 14/09/2018.
+//  Copyright © 2018 Juri. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewControllers: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = UIColor.red
+    }
+    
+}
