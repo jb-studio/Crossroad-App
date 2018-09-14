65626cb94a50f300fef884fb0c757f80b6e363fb
Juri <juri@juris-mbp-2.connect>
2018-09-14 14:24:54 +0100

    Initial Commit

---
+++ Crossroad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Crossroad
+//
+//  Created by Juri on 14/09/2018.
+//  Copyright © 2018 Juri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
